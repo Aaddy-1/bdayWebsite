e69503117d4a5a5e611490d5e2506ade10dc5d5c
Aadeesh Sharma <sharmaaadeesh@gmail.com>
2025-02-27 05:16:00 -0700

    fixed typo

--- src/components/WelcomeComponent.jsx
+++ src/components/WelcomeComponent.jsx
@@ -9,9 +9,10 @@ import "./welcome.css";
 
 const WelcomeComponent = () => {
   const songs = [
-    { title: "Call It What You Want", src: "/songs/callitWhatYouWant.mp3", thumbnail: "/images/callitWhatYouWant.png" },
-    { title: "All Too Well", src: "/songs/allTooWell.mp3", thumbnail: "/images/allTooWell.jpg" },
-    { title: "Ye Tune Kya Kiya", src: "songs/yeTuneKyaKiya.mp3", thumbnail: "/images/yeTuneKyaKiya.jpg" },
+    { title: "Yellow", src: "songs/yellow.mp3", thumbnail: "images/yellow.jpg" },
+    { title: "Call It What You Want", src: "songs/callitWhatYouWant.mp3", thumbnail: "images/callItWhatYouWant.png" },
+    { title: "All Too Well", src: "songs/allTooWell.mp3", thumbnail: "images/allTooWell.jpg" },
+    { title: "Ye Tune Kya Kiya", src: "songs/yeTuneKyaKiya.mp3", thumbnail: "images/yeTuneKyaKiya.jpg" },
   ];
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
